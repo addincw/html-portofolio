255e62fbd52f2dce6d852d59629d9c2c5efb4f8d
addin cendekia <rockydocky007@yahoo.com>
2018-12-04 22:44:53 +0700

    add smooth scroll, add transition on scroll

--- js/main.js
+++ js/main.js
@@ -5,36 +5,6 @@ function jumpSection(elementId){
   element1.classList.remove("active");
   var element1 = document.getElementsByName(elementId)[0];
   element1.classList.add("active");
-
-  smoothScroll(elementId);
-}
-function smoothScroll(elementId, currentPos = 0){
-  var target = document.getElementById(elementId).offsetTop;
-  scroller = setTimeout(function(){
-    smoothScroll(elementId, currentPos);
-  }, 1);
-
-  currentPos = currentPos + 15;
-
-  if (currentPos >= target) {
-    clearTimeout(scroller);
-  }
-
-  window.scroll(0, currentPos);
-}
-function backToTop(currentPos = lastPos){
-  scroller = setTimeout(function(){
-    backToTop(currentPos);
-  }, 1);
-
-  currentPos = currentPos - 50;
-  // console.log(elementId+" : "+currentPos);
-
-  if (currentPos <= 0) {
-    clearTimeout(scroller);
-  }
-
-  window.scroll(0, currentPos);
 }
 function trackPositionElement() {
   var currentPos = document.documentElement.scrollTop;
@@ -44,11 +14,11 @@ window.onscroll = function() {
   lastPos = trackPositionElement();
 
   if (lastPos <= 500) { //section kedua
-    document.getElementsByClassName("scrollTop")[0].style.display="none";
-    document.getElementsByClassName("navbar-folio-toogle")[0].style.display="none";
+    document.getElementsByClassName("scrollTop")[0].classList.remove("show");
+    document.getElementsByClassName("navbar-folio-toogle")[0].classList.remove("show");
   }else {
-    document.getElementsByClassName("scrollTop")[0].style.display="block";
-    document.getElementsByClassName("navbar-folio-toogle")[0].style.display="block";
+    document.getElementsByClassName("scrollTop")[0].classList.add("show");
+    document.getElementsByClassName("navbar-folio-toogle")[0].classList.add("show");
   }
 
 };
